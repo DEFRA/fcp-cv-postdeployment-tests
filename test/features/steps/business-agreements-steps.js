a999d86dcd8f5c33a3f967b8543e14d7dc239e04
gmarshd <gordon.marsh@defra.gov.uk>
2026-01-27 11:35:59 +0000

    Initial agreements functions

--- test/features/steps/business-agreements-steps.js
+++ test/features/steps/business-agreements-steps.js
@@ -1,6 +1,5 @@
 import { createBdd } from 'playwright-bdd'
 import { test } from './fixtures.js'
-import { BusinessAgreementsPage } from '../page-objects/business-agreements-page.js'
 
 const { When, Then } = createBdd(test)
 
@@ -8,10 +7,12 @@ const { When, Then } = createBdd(test)
 // UI Components on Agreements Page
 // ------------------------------------------------------
 
-Then(/^each Agreements table has an option to view the record$/, function () {
-  // TODO
-  BusinessAgreementsPage.checkTitle()
-})
+Then(
+  /^each Agreements table has an option to view the record$/,
+  function ({ businessAgreementsPage }) {
+    businessAgreementsPage.checkViewRecordIsAvailable()
+  }
+)
 
 // ------------------------------------------------------
 // Agreement Details Page Components
@@ -19,25 +20,28 @@ Then(/^each Agreements table has an option to view the record$/, function () {
 
 When(
   /^I click the 'View' link next to (?:an agreement|the agreement with reference '(\d+)')$/,
-  function (ref) {
+  function ({ businessAgreementsPage }, ref) {
     // TODO
   }
 )
 
-Then(/^I see a header with 'Agreement Name' as the title$/, function () {
-  // TODO
-})
+Then(
+  /^I see a header with 'Agreement Name' as the title$/,
+  function ({ businessAgreementsPage }) {
+    // TODO
+  }
+)
 
 Then(
   /^I see underneath the header the following fields '(.+)'$/,
-  function (fields) {
+  function ({ businessAgreementsPage }, fields) {
     // TODO
   }
 )
 
 Then(
   /^I see an option to go back to see the main screen with Agreements table$/,
-  function () {
+  function ({ businessAgreementsPage }) {
     // TODO
   }
 )
@@ -46,14 +50,17 @@ Then(
 // Back Navigation
 // ------------------------------------------------------
 
-When(/^I click the 'Back to Agreements list' link$/, function () {
-  // TODO
-})
+When(
+  /^I click the 'Back to Agreements list' link$/,
+  function ({ businessAgreementsPage }) {
+    // TODO
+  }
+)
 
 // Generic "correct list of Agreements" result table
 Then(
   /^I see the the correct list of Agreements as follows$/,
-  function (dataTable) {
+  function ({ businessAgreementsPage }, dataTable) {
     // TODO
   }
 )
@@ -64,7 +71,7 @@ Then(
 
 Then(
   /^I see the correct data in the Agreements table, ordered by most recent year first, as follows$/,
-  function (dataTable) {
+  function ({ businessAgreementsPage }, dataTable) {
     // TODO
   }
 )
@@ -75,21 +82,21 @@ Then(
 
 Then(
   /^I see the Agreement Details table ordered by 'Sheet', 'Parcel', 'Description', 'Payment Schedule' alphabetically ascending$/,
-  function (dataTable) {
+  function ({ businessAgreementsPage }, dataTable) {
     // TODO
   }
 )
 
 Then(
   /^The screen data for Action Area \(ha\) in the Payment Schedule selected is the amount in the JSON divided by 10,000 as it is provided in m2 but displayed in ha\.$/,
-  function () {
+  function ({ businessAgreementsPage }) {
     // TODO
   }
 )
 
 Then(
   /^The screen data for Parcel Area \(ha\) in the Payment Schedule selected is the amount in the JSON divided by 10,000 as it is provided in m2 but displayed in ha\.$/,
-  function () {
+  function ({ businessAgreementsPage }) {
     // TODO
   }
 )
--- test/page-objects/business-agreements-page.js
+++ test/page-objects/business-agreements-page.js
@@ -3,9 +3,18 @@ import { expect } from '@playwright/test'
 export default class BusinessAgreementsPage {
   constructor(page) {
     this.page = page
+    this.agreementsTable = page.getByTestId('agreements-table')
   }
 
   async checkTitle() {
     await expect(this.page).toHaveTitle('Business Agreements')
+  }
+
+  async checkViewRecordIsAvailable() {
+    const rows = await this.agreementsTable.locator('tr')
+    await rows.forEach(() => {
+      const firstCell = this.permissionsDescriptionsTable.locator('td').first()
+      expect(firstCell.getByRole('link')).toHaveCount(1)
+    })
   }
 }
